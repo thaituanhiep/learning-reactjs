ddb3628e87b9c88a3e812e0a1b10a261a8807ccd
hieptt8 <hieptt8@fpt.com.vn>
2022-06-10 14:51:09 +0700

    fix app js

--- source/lesson01-helloreact/src/App.js
+++ source/lesson01-helloreact/src/App.js
@@ -3,28 +3,28 @@ import "./App.css";
 import React, { Component } from "react";
 
 class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-				</header>{" "}
-				<h2>Hra</h2>
-				<p className="App-intro">
-					Để bắt đầu học vui lòng truy cập <code> src / App.js </code> chỉnh sửa
-					và lưu lại.{" "}
-				</p>{" "}
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React{" "}
-				</a>{" "}
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>{" "}
+        <h2>Hra</h2>
+        <p className="App-intro">
+          Để bắt đầu học vui lòng truy cập <code> src / App.js </code> chỉnh sửa
+          và lưu lại.{" "}
+        </p>{" "}
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React{" "}
+        </a>{" "}
+      </div>
+    );
+  }
 }
 
 export default App;
